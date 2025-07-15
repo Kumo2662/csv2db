94eae42ef298b18d6101ce29e399b3622e2f8f90
Kumo2662 <tk26625106@gmail.com>
2025-07-16 04:27:52 +0900

    add simple validation and show error count

--- app/controllers/property_controller.rb
+++ app/controllers/property_controller.rb
@@ -7,16 +7,30 @@ class PropertyController < ApplicationController
       if uploaded_file.content_type == 'text/csv' || File.extname(uploaded_file.original_filename).downcase == '.csv'
         properties = []
         building_type_map = { 'アパート' => 0, '一戸建て' => 1, 'マンション' => 2 }
+        required_count = 0
+        skipped_count = 0
         CSV.foreach(uploaded_file.path, headers: true, encoding: 'UTF-8') do |row|
+          unique_id = row['ユニークID']
+          name = row['物件名']
+          building_type = row['建物の種類']
+          room_number = row['部屋番号']
+          # 必須項目チェック
+          if unique_id.blank? || name.blank? || (room_number.blank? && building_type != '一戸建て')
+            skipped_count += 1
+            next
+          end
+          # 一戸建ての場合は部屋番号をNULL
+          room_number = nil if building_type == '一戸建て' && room_number.blank?
           properties << {
-            id: row['ユニークID'],
-            name: row['物件名'],
+            id: unique_id,
+            name: name,
             address: row['住所'],
-            room_number: row['部屋番号'],
+            room_number: room_number,
             rent: row['賃料'],
             area: row['広さ'],
-            building_type: building_type_map[row['建物の種類']]
+            building_type: building_type_map[building_type]
           }
+          required_count += 1
         end
         updated_count = 0
         Csv2db::Property.transaction do
@@ -25,7 +39,7 @@ class PropertyController < ApplicationController
             updated_count += result.length
           end
         end
-        flash[:notice] = "登録・更新:#{updated_count}件"
+        flash[:notice] = "登録・更新:#{updated_count}件、エラー:#{skipped_count}件"
       else
         flash[:alert] = 'only CSV files are allowed'
       end
