54525531fbdf4e29264900e9a24a093ab5586671
Kumo2662 <tk26625106@gmail.com>
2025-07-16 20:52:22 +0900

    set ja.yml

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,8 @@ module Csv2db
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # 设置默认语言为日语
+    config.i18n.default_locale = :ja
   end
 end
